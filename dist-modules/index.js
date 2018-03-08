72710b8e494b481eb601acca64f8a2ce621ec06a
MrNav <viet.na@neo-lab.vn>
2018-03-08 11:50:38 +0700

    format code and change some line codes

--- dist-modules/index.js
+++ dist-modules/index.js
@@ -1,19 +1,22 @@
 'use strict';
 
-module.exports = function (str, maxChars, options) {
-  var _optionsDefault = {
+module.exports = (str, maxChars, options) => {
+  const _optionsDefault = {
     wordBoundary: true,
     chars: [' ', '-'],
     endSymbols: '...'
   };
-
-  var appendEndSymbols = function appendEndSymbols(strlen, maxChars, endSymbols) {
-    return strlen > maxChars ? endSymbols : '';
-  };
+  const charRegx = /\s* \s*|\s*-\s*/;
+  const words = str.split(charRegx);
+  let retStr = '';
 
   options = Object.assign(_optionsDefault, options);
-  options.charRegx = /\s* \s*|\s*-\s*/;
-  if (str.length <= maxChars) {
+
+  function appendEndSymbols(strlen, maxChars, endSymbols) {
+    return strlen > maxChars ? endSymbols : '';
+  }
+
+  if (!maxChars || str.length <= maxChars) {
     return str;
   }
 
@@ -21,18 +24,11 @@ module.exports = function (str, maxChars, options) {
     return str.substring(0, maxChars) + appendEndSymbols(str.length, maxChars, options.endSymbols);
   }
 
-  var words = str.split(options.charRegx);
-
-  var retStr = '';
-  for (var i = 0; i < words.length; i++) {
-    if ((retStr + ' ' + words[i]).length > maxChars) {
+  for (let i = 0; i < words.length; i++) {
+    if (`${retStr} ${words[i]}`.length > maxChars) {
       return str.substring(0, retStr.length) + appendEndSymbols(str.length, retStr.length, options.endSymbols);
     } else {
-      if (i === 0) {
-        retStr = words[0];
-      } else {
-        retStr += ' ' + words[i];
-      }
+      retStr = i === 0 ? words[0] : retStr + ` ${words[i]}`;
     }
   }
 };
